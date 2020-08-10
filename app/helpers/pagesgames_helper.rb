c8ea108452d3aa9b9ed2a07868522d143f37e95d
thomas viaules <thomas.viaules@gmail.com>
2020-08-10 16:47:20 -0400

    challenge 3 in progress

---
+++ app/helpers/pagesgames_helper.rb
@@ -0,0 +1,2 @@
+module PagesgamesHelper
+end
---
+++ test/controllers/pagesgames_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PagesgamesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
